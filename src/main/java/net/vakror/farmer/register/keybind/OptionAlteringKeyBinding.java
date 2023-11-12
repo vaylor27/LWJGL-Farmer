a8f1823d5aeda59e0d6c14baa00d85dca0f25682
vaylor27 <godisagodlol@gmail.com>
2023-11-11 19:49:02 -0500

    add OptionAlteringKeyBinding

---
+++ src/main/java/net/vakror/farmer/register/keybind/OptionAlteringKeyBinding.java
@@ -0,0 +1,31 @@
+package net.vakror.farmer.register.keybind;
+
+import net.vakror.farmer.FarmerGameMain;
+import net.vakror.farmer.register.option.Option;
+
+import java.util.function.Consumer;
+
+public class OptionAlteringKeyBinding<T extends Option<?>> extends KeyBinding {
+    private final Consumer<T> onExecute;
+    private final T optionToAlter;
+    private final boolean shouldReloadMatrices;
+
+    public OptionAlteringKeyBinding(int key, T optionToAlter, Consumer<T> onExecute, boolean shouldReloadMatrices) {
+        super(key);
+        this.onExecute = onExecute;
+        this.optionToAlter = optionToAlter;
+        this.shouldReloadMatrices = shouldReloadMatrices;
+    }
+
+    public T optionToAlter() {
+        return optionToAlter;
+    }
+
+    @Override
+    public void execute(int scancode, int action, int mods) {
+        onExecute.accept(optionToAlter);
+        if (shouldReloadMatrices) {
+            FarmerGameMain.renderer.regenProjectionMatrix();
+        }
+    }
+}
